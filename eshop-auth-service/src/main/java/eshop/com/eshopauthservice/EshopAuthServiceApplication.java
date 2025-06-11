d479abdcea456b4edd84e316cdb0e19cdb4bf5a6
Konstantin <knetsov91@gmail.com>
2025-06-11 15:32:20 +0300

    Init Spring Boot auth microservice project.

---
+++ eshop-auth-service/src/main/java/eshop/com/eshopauthservice/EshopAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package eshop.com.eshopauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopAuthServiceApplication.class, args);
+    }
+
+}
---
+++ eshop-auth-service/src/test/java/eshop/com/eshopauthservice/EshopAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eshop.com.eshopauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
