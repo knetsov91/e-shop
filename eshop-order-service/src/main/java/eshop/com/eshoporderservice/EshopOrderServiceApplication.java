13e211ef0da763a2617d8cba511744cd40e379fc
Konstantin <knetsov91@gmail.com>
2025-06-11 15:42:25 +0300

    Init Spring Boot order microservice project.

---
+++ eshop-order-service/src/main/java/eshop/com/eshoporderservice/EshopOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package eshop.com.eshoporderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopOrderServiceApplication.class, args);
+    }
+
+}
