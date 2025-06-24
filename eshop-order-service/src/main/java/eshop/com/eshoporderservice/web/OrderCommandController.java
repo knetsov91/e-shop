06bc80cbfb3bed937484e6beae1c22f339cc8510
Konstantin <knetsov91@gmail.com>
2025-06-24 13:12:56 +0300

    Order microservice - change OrderCommandController's createOrderCommand handler instead entity to accept DTO and add validation for it.

--- eshop-order-service/src/main/java/eshop/com/eshoporderservice/web/OrderCommandController.java
+++ eshop-order-service/src/main/java/eshop/com/eshoporderservice/web/OrderCommandController.java
@@ -2,6 +2,8 @@ package eshop.com.eshoporderservice.web;
 
 import eshop.com.eshoporderservice.order.model.OrderCommand;
 import eshop.com.eshoporderservice.service.OrderCommandService;
+import eshop.com.eshoporderservice.web.dto.OrderCommandCreateRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +21,8 @@ public class OrderCommandController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderCommand> createOrderCommand(@RequestBody  OrderCommand orderCommand) {
-        OrderCommand order = orderCommandService.createOrder(orderCommand);
+    public ResponseEntity<OrderCommand> createOrderCommand(@Valid @RequestBody OrderCommandCreateRequest orderCommandRequest) {
+        OrderCommand order = orderCommandService.createOrder(orderCommandRequest);
         return ResponseEntity.ok().body(order);
     }
 }
