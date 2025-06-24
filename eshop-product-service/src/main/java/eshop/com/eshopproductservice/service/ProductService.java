d212ba9d32213f0b3a8a18058e4eee8a592bad6f
Konstantin <knetsov91@gmail.com>
2025-06-25 00:25:57 +0300

    Product microservice - add getProducts() in ProductService.

---
+++ eshop-product-service/src/main/java/eshop/com/eshopproductservice/service/ProductService.java
@@ -0,0 +1,20 @@
+package eshop.com.eshopproductservice.service;
+
+import eshop.com.eshopproductservice.model.Product;
+import eshop.com.eshopproductservice.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+}
