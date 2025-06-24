9910cc13828c1271cd87596dbb569009df6b6f6f
Konstantin <knetsov91@gmail.com>
2025-06-24 16:20:43 +0300

    Order microservice - use Lombok's Data annotation for getters and setters in OrderQuery entity.

--- eshop-order-service/src/main/java/eshop/com/eshoporderservice/order/model/OrderQuery.java
+++ eshop-order-service/src/main/java/eshop/com/eshoporderservice/order/model/OrderQuery.java
@@ -2,8 +2,10 @@ package eshop.com.eshoporderservice.order.model;
 
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @AllArgsConstructor
 @Document(collection = "orders")
 public class OrderQuery {
