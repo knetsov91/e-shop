4b6bc96dce8497c1b0d97561d64240bd25508ce6
Konstantin <knetsov91@gmail.com>
2025-06-25 00:42:38 +0300

    Product microservice - add createProduct() in ProductService.

--- eshop-product-service/src/main/java/eshop/com/eshopproductservice/service/ProductService.java
+++ eshop-product-service/src/main/java/eshop/com/eshopproductservice/service/ProductService.java
@@ -2,6 +2,7 @@ package eshop.com.eshopproductservice.service;
 
 import eshop.com.eshopproductservice.model.Product;
 import eshop.com.eshopproductservice.repository.ProductRepository;
+import eshop.com.eshopproductservice.web.dto.ProductCreateRequest;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -16,5 +17,14 @@ public class ProductService {
 
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    public Product createProduct(ProductCreateRequest  productCreateRequest) {
+        Product product = new Product();
+        product.setName(productCreateRequest.getName());
+        product.setPrice(productCreateRequest.getPrice());
+        product.setQuantity(productCreateRequest.getQuantity());
+
+        return productRepository.save(product);
     }
 }
