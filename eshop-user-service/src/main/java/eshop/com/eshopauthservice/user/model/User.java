0bbec46f7e4d96b742c867e184588e616d041b39
Konstantin <knetsov91@gmail.com>
2025-07-04 16:56:14 +0300

    User microservice - fix typos and change updatedAt and createdAt fields types to LocalDataTime in User entity.

--- eshop-user-service/src/main/java/eshop/com/eshopauthservice/user/model/User.java
+++ eshop-user-service/src/main/java/eshop/com/eshopauthservice/user/model/User.java
@@ -1,7 +1,7 @@
 package eshop.com.eshopauthservice.user.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="users")
@@ -17,7 +17,55 @@ public class User {
 
     private String email;
 
-    private LocalDate createdAd;
+    private LocalDateTime createdAt;
 
-    private LocalDate updatedAd;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
