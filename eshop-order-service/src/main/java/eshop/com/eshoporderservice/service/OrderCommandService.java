5967df8c8ebca75e080fa3f81d84c87b837f6fc5
Konstantin <knetsov91@gmail.com>
2025-07-05 17:50:47 +0300

    Order microservice - change OrderCommandService's createOrder() argument to OrderCommandCreateRequest.

--- eshop-order-service/src/main/java/eshop/com/eshoporderservice/service/OrderCommandService.java
+++ eshop-order-service/src/main/java/eshop/com/eshoporderservice/service/OrderCommandService.java
@@ -2,6 +2,7 @@ package eshop.com.eshoporderservice.service;
 
 import eshop.com.eshoporderservice.order.model.OrderCommand;
 import eshop.com.eshoporderservice.order.repository.OrderCommandRepository;
+import eshop.com.eshoporderservice.web.dto.OrderCommandCreateRequest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,12 @@ public class OrderCommandService {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public OrderCommand createOrder(OrderCommand orderCommand) {
-        OrderCommand save = orderCommandRepository.save(orderCommand);
+    public OrderCommand createOrder(OrderCommandCreateRequest orderCommand) {
+        OrderCommand orderCommandEntity = new OrderCommand();
+        orderCommand.setProduct(orderCommand.getProduct());
+        orderCommand.setQuantity(orderCommand.getQuantity());
+
+        OrderCommand save = orderCommandRepository.save(orderCommandEntity);
         kafkaTemplate.send("order-events", "OrderCreated:%s".formatted(save.getId()));
         return save;
     }
