e68218751b362c42635fe582130c4dea0850f044
User <knetsov91@gmail.com>
2026-05-18 18:07:17 +0300

    fix(order-service): Fix OrderCommandService to accept DTO instead of entity

--- eshop-order-service/src/main/java/eshop/com/eshoporderservice/service/OrderCommandService.java
+++ eshop-order-service/src/main/java/eshop/com/eshoporderservice/service/OrderCommandService.java
@@ -2,13 +2,14 @@ package eshop.com.eshoporderservice.service;
 
 import eshop.com.eshoporderservice.order.model.OrderCommand;
 import eshop.com.eshoporderservice.order.repository.OrderCommandRepository;
+import eshop.com.eshoporderservice.web.dto.OrderCommandCreateRequest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderCommandService {
 
-    private OrderCommandRepository  orderCommandRepository;
+    private OrderCommandRepository orderCommandRepository;
 
     private KafkaTemplate<String, String> kafkaTemplate;
 
@@ -17,9 +18,14 @@ public class OrderCommandService {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public OrderCommand createOrder(OrderCommand orderCommand) {
-        OrderCommand save = orderCommandRepository.save(orderCommand);
-        kafkaTemplate.send("order-events", "OrderCreated:%s".formatted(save.getId()));
-        return save;
+    public OrderCommand createOrder(OrderCommandCreateRequest request) {
+        OrderCommand orderCommand = new OrderCommand();
+        orderCommand.setProduct(request.getProduct());
+        orderCommand.setQuantity(request.getQuantity());
+        orderCommand.setStatus("PENDING");
+
+        OrderCommand saved = orderCommandRepository.save(orderCommand);
+        kafkaTemplate.send("order-events", "OrderCreated:%s".formatted(saved.getId()));
+        return saved;
     }
 }
