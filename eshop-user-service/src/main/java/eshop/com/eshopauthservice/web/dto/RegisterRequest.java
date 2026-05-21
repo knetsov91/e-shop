6a92e0b55413879af8c6753d6cf60caeb6fdf054
User <knetsov91@gmail.com>
2026-05-22 00:03:49 +0300

    feat(user-service): add RegisterRequest DTO with validation

---
+++ eshop-user-service/src/main/java/eshop/com/eshopauthservice/web/dto/RegisterRequest.java
@@ -0,0 +1,26 @@
+package eshop.com.eshopauthservice.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class RegisterRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
