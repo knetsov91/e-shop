30af08898cda3b31d2aeea68351654437af059c6
User <knetsov91@gmail.com>
2026-05-21 23:13:50 +0300

    feat(user-service): add UserRepository with findByUsername

---
+++ eshop-user-service/src/main/java/eshop/com/eshopauthservice/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package eshop.com.eshopauthservice.user.repository;
+
+import eshop.com.eshopauthservice.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
