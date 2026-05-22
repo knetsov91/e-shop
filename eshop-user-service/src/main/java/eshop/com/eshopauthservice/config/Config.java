cbf3f60f1d29df90f1ad5c128858522a4a18da5e
User <knetsov91@gmail.com>
2026-05-22 19:12:45 +0300

    feat(user-service): add RestTemplate bean for HTTP calls

--- eshop-user-service/src/main/java/eshop/com/eshopauthservice/config/Config.java
+++ eshop-user-service/src/main/java/eshop/com/eshopauthservice/config/Config.java
@@ -3,6 +3,7 @@ package eshop.com.eshopauthservice.config;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Config {
@@ -10,5 +11,10 @@ public class Config {
     @Bean
     public InMemoryHttpExchangeRepository inMemoryHttpExchangeRepository() {
         return new InMemoryHttpExchangeRepository();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
